4363c1cc5ddbc653e715da77bb5605c01b47d1ea
Romain Graillot <graillot.romain@gmail.com>
2025-05-05 15:01:42 +0200

    Adds selected text background color

--- src/core/theme/ColorId.h
+++ src/core/theme/ColorId.h
@@ -8,17 +8,18 @@
  * These identifiers are used to access theme colors for UI components.
  */
 enum class ColorId {
-    MarginBackground,     ///< Background color of the margin area (line number container).
-    LineBackground,       ///< Background color for the current (at cursor position) text lines.
-    LineNumber,           ///< Color for the line numbers.
-    InfoBarBackground,    ///< Background color of the info bar.
-    EditorBackground,     ///< Background color of the editor area.
-    PromptBackground,     ///< Background color of the command prompt.
-    InfoBarText,          ///< Text color in the info bar.
-    PromptText,           ///< Text color for static prompt messages.
-    PromptInputText,      ///< Text color for user input in the prompt.
-    Border,               ///< Color used for borders (e.g. between components).
-    CursorIndicator       ///< Color of the cursor indicator.
+    MarginBackground,       ///< Background color of the margin area (line number container).
+    LineBackground,         ///< Background color for the current (at cursor position) text lines.
+    SelectedTextBackground, ///< Background color for selected text range.
+    LineNumber,             ///< Color for the line numbers.
+    InfoBarBackground,      ///< Background color of the info bar.
+    EditorBackground,       ///< Background color of the editor area.
+    PromptBackground,       ///< Background color of the command prompt.
+    InfoBarText,            ///< Text color in the info bar.
+    PromptText,             ///< Text color for static prompt messages.
+    PromptInputText,        ///< Text color for user input in the prompt.
+    Border,                 ///< Color used for borders (e.g. between components).
+    CursorIndicator         ///< Color of the cursor indicator.
 };
 
 
