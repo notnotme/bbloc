3ff956b1537196242ae5cbff8333db73b1e21ff1
Romain Graillot <graillot.romain@gmail.com>
2025-05-25 23:00:39 +0200

    TextRange struct should be final
    (cherry picked from commit 2eb143a46c741ca515a1e657276c3a2e290da0c0)

--- src/core/cursor/TextRange.h
+++ src/core/cursor/TextRange.h
@@ -3,7 +3,7 @@
 
 
 /** @brief Represents a range of text by line, column position. */
-struct TextRange {
+struct TextRange final {
     uint32_t line_start;     ///< The line where the range starts
     uint32_t column_start;   ///< The column where the range starts
     uint32_t line_end;       ///< The line where the range ends
